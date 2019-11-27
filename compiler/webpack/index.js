418f3b2898ef89c545636888162aa0aad52c7d00
newbee1111 <799723513@qq.com>
2019-11-27 17:59:21 +0800

    upd: 删除getHtmlPlugin

--- compiler/webpack/index.js
+++ compiler/webpack/index.js
@@ -3,13 +3,11 @@ const devConfig = require('./webpack.dev.config')
 const prodConfig = require('./webpack.prod.config')
 const compileDev = require('../build/build.dev')
 const compileProd = require('../build/build.prod')
-const getHtmlPlugin = require('./utils/htmlPlugins')
 
 module.exports = {
   baseConfig,
   devConfig,
   prodConfig,
   compileDev,
-  compileProd,
-  getHtmlPlugin
+  compileProd
 }
