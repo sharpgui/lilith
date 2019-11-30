61a2d8f66a51a01c96df2687991a269bc39d9ab5
advence-liz <advence_liz@163.com>
2019-11-30 17:38:50 +0800

    upd: 添加根据模板创建模块功能

---
+++ config.js
@@ -0,0 +1,29 @@
+const debug = require('debug')('lilith:cli')
+const fs = require('fs')
+const findup = require('findup-sync')
+const path = require('path')
+let context = process.cwd()
+let root = '_template'
+let config = {}
+
+// context 优先级 qt.config.js > package.json > cwd
+try {
+    context = path.dirname(findup('package.json'))
+} catch (error) {}
+try {
+    const configFilePath = findup('lilith.js')
+    context = path.dirname(configFilePath)
+    config = require(path.resolve(configFilePath))
+} catch (error) {
+    debug(error)
+}
+if (fs.existsSync(path.resolve(context, '_template'))) {
+    root = '_template'
+}
+
+module.exports = {
+    ...{ rename: false, filter: {} },
+    context,
+    root,
+    ...config
+}
