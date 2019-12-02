9ac51caa33980abcbf645eaee82fa63fda3821b1
advence-liz <advence_liz@163.com>
2019-12-02 21:49:47 +0800

    upd: 添加强制代码规范约束

--- config.js
+++ config.js
@@ -8,22 +8,24 @@ let config = {}
 
 // context 优先级 qt.config.js > package.json > cwd
 try {
-    context = path.dirname(findup('package.json'))
-} catch (error) {}
-try {
-    const configFilePath = findup('lilith.js')
-    context = path.dirname(configFilePath)
-    config = require(path.resolve(configFilePath))
+  context = path.dirname(findup('package.json'))
 } catch (error) {
-    debug(error)
+  debug('没有找到package.json')
+}
+try {
+  const configFilePath = findup('lilith.js')
+  context = path.dirname(configFilePath)
+  config = require(path.resolve(configFilePath))
+} catch (error) {
+  debug(error)
 }
 if (fs.existsSync(path.resolve(context, '_template'))) {
-    root = '_template'
+  root = '_template'
 }
 
 module.exports = {
-    ...{ rename: false, filter: {} },
-    context,
-    root,
-    ...config
+  ...{ rename: false, filter: {} },
+  context,
+  root,
+  ...config
 }
