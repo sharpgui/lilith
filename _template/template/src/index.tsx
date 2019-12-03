4efe6e77d952149060b38867f395a427d3957614
advence-liz <advence_liz@163.com>
2019-12-03 13:55:37 +0800

    upd: 暂且添加_template 目录

---
+++ _template/template/src/index.tsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+import { getData } from './services'
+
+
+export default class {{name|pascal}} extends Component {
+  componentDidMount() {
+    getData().then(res => {
+      console.log(res)
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        Hello World !
+      </div>
+    )
+  }
+}
+
+render(<{{name|pascal}}></{{name|pascal}}>, document.getElementById('root'))
