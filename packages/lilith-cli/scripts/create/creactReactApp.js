a910ce4f7ba5f7fdb7c6566748f4b7644027b952
lizhuo13 <lizhuo13@meituan.com>
2020-06-22 17:28:17 +0800

    feat: 添加下载临时路径

--- packages/lilith-cli/scripts/create/creactReactApp.js
+++ packages/lilith-cli/scripts/create/creactReactApp.js
@@ -4,7 +4,7 @@ const fs = require('fs-extra')
 const path = require('path')
 const logger = require('../../lib/logger')
 
-const downloadTemplate = function (path) {
+const downloadTemplate = function(path) {
   shell.exec('git init', { cwd: path })
   shell.exec(
     'git remote add -f origin git@github.com:facebook/create-react-app.git',
@@ -12,16 +12,16 @@ const downloadTemplate = function (path) {
   )
   shell.exec('git config core.sparsecheckout true', { cwd: path })
   shell.exec('echo packages/cra-template >> .git/info/sparse-checkout', {
-    cwd: path,
+    cwd: path
   })
   shell.exec('git pull origin master', { cwd: path })
 }
 
-module.exports = async () => {
-  const path = '/Users/qudian/Downloads/lili1'
-  fs.ensureDirSync(path)
-  await downloadTemplate(path)
-  await fs.copy(path + '/packages/cra-template', path, () => {
-    fs.remove(path + '/packages')
+module.exports = async name => {
+  const tempPath = path.resolve('.lilith-temp')
+  fs.ensureDirSync(tempPath)
+  await downloadTemplate(tempPath)
+  await fs.copy(tempPath + '/packages/cra-template', path.resolve(name), () => {
+    fs.remove(tempPath + '/packages')
   })
 }
