397412bb3dc250dc11956f622320e378d8f32d9b
advence-liz <advence_liz@163.com>
2021-07-22 22:25:03 +0800

    feat: 更新模板

--- packages/lilith-cli/_template/react/src/index.jsx
+++ packages/lilith-cli/_template/react/src/index.jsx
@@ -1,9 +1,15 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
+import Button from './components/button'
 
 export default class App extends Component {
   render() {
-    return <div>Hello World ! </div>
+    return (
+      <div>
+        Hello World !
+        <Button />
+      </div>
+    )
   }
 }
 
--- packages/lilith-cli/_template/vue/src/app.vue
+++ packages/lilith-cli/_template/vue/src/app.vue
@@ -2,11 +2,14 @@
   <div id="app">
     <h1>vue</h1>
     hello world!!!
+    <Button />
   </div>
 </template>
 
 <script>
+import Button from './components/button'
 export default {
+  components:{Button},
   name: 'app'
 }
 </script>
